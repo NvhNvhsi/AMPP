b2c7f19e718e1390b8743d65f9b8fa327d443a28
Nathan VH <n.vanhollebeke@nvhsi.fr>
2023-11-19 15:39:02 +0000

    message Changes to be committed: 	modified:   .env 	modified:   docker-compose.yml 	modified:   www/index.php

--- www/index.php
+++ www/index.php
@@ -1,20 +1,7 @@
 <?php
-$servername = "ampp-mysql"; // Nom du container MySQL (adapté au projet AMPP)
-$username = "myuser"; // Utilisateur MySQL (adapté au projet AMPP)
-$password = "azerty"; // Mot de passe MySQL (adapté au projet AMPP)
-$database = "mydatabase"; // Nom de la base de données MySQL (adapté au projet AMPP)
+// Affiche toutes les informations, comme le ferait INFO_ALL
+echo 'lol';
 
-// Connexion à la base de données
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("La connexion à la base de données a échoué : " . $conn->connect_error);
-}
-
-echo "Connexion à la base de données réussie.";
-
-// Fermer la connexion
-$conn->close();
 ?>
 
+<h1> LOL o</h1>
