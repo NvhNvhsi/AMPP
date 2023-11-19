3ea14e09efbacc593b252cacb37213d511113aa6
NvhNvhsi <151438235+NvhNvhsi@users.noreply.github.com>
2023-11-19 15:45:16 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,20 @@
+<?php
+$servername = "ampp-mysql"; // Nom du container MySQL (adapté au projet AMPP)
+$username = "myuser"; // Utilisateur MySQL (adapté au projet AMPP)
+$password = "azerty"; // Mot de passe MySQL (adapté au projet AMPP)
+$database = "mydatabase"; // Nom de la base de données MySQL (adapté au projet AMPP)
+
+// Connexion à la base de données
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion à la base de données a échoué : " . $conn->connect_error);
+}
+
+echo "Connexion à la base de données réussie.";
+
+// Fermer la connexion
+$conn->close();
+?>
 
